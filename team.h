35ec82c18ea78b7d42a496c7d8c2d1cd6b02b380
hp <hebaadbelsttar7@gmail.com>
2023-08-02 02:25:31 +0300

    try

---
+++ team.h
@@ -0,0 +1,32 @@
+#ifndef TEAM_H_
+#define TEAM_H_
+
+
+typedef struct footballteam
+{
+int established_year;
+char rank_in_league;
+char captain_name [20];
+}ft;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // TEAM_H_
